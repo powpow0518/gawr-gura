0000bec55a038bcf97d9192c2aacd08df90811d0
minliu10 <serendipitous251@gmail.com>
2020-11-16 11:46:12 +0800

    頻道描述改成捲軸文字框

--- project/SingleResult.py
+++ project/SingleResult.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import Globals
-
+from tkinter.scrolledtext import ScrolledText
 
 def showtheresult(channel_name, subscribes, desc):  # 怪怪的
 
@@ -24,23 +24,35 @@ def showtheresult(channel_name, subscribes, desc):  # 怪怪的
 
     Label_banner.grid(row=0, column=0, columnspan=2, sticky='NEW')
 
-    profile_photo = ImageTk.PhotoImage(Image.open(photo_forlder + photo_name + "_profile.jpg"))
+    profile = Image.open(photo_forlder + photo_name + "_profile.jpg")
+    new_hw = int(banner_img.height * 1.1)
+    prifile_resize = profile.resize((new_hw, new_hw),Image.ANTIALIAS)
+    profile_photo = ImageTk.PhotoImage(prifile_resize)
 
     Label_profile = Label(root,
                           anchor='sw',
                           image=profile_photo)
 
-    Label_profile.grid(row=1, column=0, rowspan=3,
-                       padx=30, pady=30, sticky="WS")
+    Label_profile.grid(row=1, column=0, rowspan=4,
+                       padx=20, pady=20, sticky="WS")
 
     Label_text1 = Label(root, text="頻道名稱：" + channel_name)
     Label_text1.grid(row=1, column=1)
 
     Label_text2 = Label(root, text="訂閱數：" + subscribes)
     Label_text2.grid(row=2, column=1)
+    
+    Label_text3 = Label(root, text="頻道敘述：")
+    Label_text3.grid(row=3, column=1,sticky="WS")
+    text_area =ScrolledText(root,  
+                                        wrap = WORD,  
+                                        width = 45,  
+                                        height = 4,  
+                                        font = ("Times New Roman", 12)) 
+    text_area.grid(row=4, column=1,sticky="NW")
+    text_area.insert(INSERT, desc)
+    
 
-    Label_text3 = Label(root, text="頻道敘述：" + desc)
-    Label_text3.grid(row=3, column=1)
 
     root.resizable(width=0, height=0)
     root.mainloop()
