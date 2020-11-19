4843a507ecabd00b21c4377b75a67e16106bf186
powpow <andy20070518@gmail.com>
2020-11-19 16:08:10 +0800

    treeview
    AAAAAAAAAAA

--- test/AAAAAAAAAAAAAA.py
+++ test/AAAAAAAAAAAAAA.py
@@ -1,40 +1,45 @@
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.scrolledtext import ScrolledText
-from tkinter import ttk
+from tkinter.ttk import *
 
 
 root = Tk()
 root.config(bg="white")
-# 設定視窗大小
-np = 125
-npdxy = 25
-root.geometry('%dx%d' % (500, (np+2*npdxy+5)*2))
-# 分割線
-sh = ttk.Separator(root, orient='horizontal')
-sh.grid(row=0, ipadx=300, column=0, columnspan=1000, sticky="we")
-# 第一章圖
-channel_img = Image.open('gura.jpg')
-channel_img = channel_img.resize((np, np), Image.ANTIALIAS)
-channel_photo0 = ImageTk.PhotoImage(channel_img)
-Label(root, image=channel_photo0,).grid(
-    row=1, column=0, rowspan=2, columnspan=2, padx=npdxy, pady=npdxy)
-Label(root, text="頻道名稱：" + 'Gura', justify='left').grid(row=1, column=2, padx=10)
-Label(root, text="頻道訂閱數：" + '1380000',
-      justify='left').grid(row=1, column=4, padx=20)
-Label(root, text="頻道影片數：" + '50', justify='left').grid(row=2, column=2, padx=10)
-Label(root, text="頻道總觀看量：" + '50000000',
-      justify='left').grid(row=2, column=4, padx=20)
-# 分割線
-sh = ttk.Separator(root, orient='horizontal')
-sh.grid(row=3, ipadx=300, column=0, columnspan=1000, sticky="we")
-# 第二章圖
-channel_img = Image.open('gura.jpg')
-channel_img = channel_img.resize((np, np), Image.ANTIALIAS)
-channel_photo1 = ImageTk.PhotoImage(channel_img)
-Label(root, image=channel_photo1,).grid(
-    row=4, column=0, rowspan=2, columnspan=2, padx=npdxy, pady=npdxy)
 
+Style().configure('Treeview', rowheight=100)
+
+# 建立treeview
+tree = Treeview(root, columns=('subs', 'videos', 'viewers'))
+# 建立標題欄
+tree.heading('#0', text='頻道名稱')
+tree.heading('#1', text='頻道訂閱數量')
+tree.heading('#2', text='上傳影片數量')
+tree.heading('#3', text='總觀看數量')
+# 格式化欄位
+tree.column('#0', width=300)
+tree.column('#1', anchor=CENTER, width=150)
+tree.column('#2', anchor=CENTER, width=100)
+tree.column('#3', anchor=CENTER, width=150)
+# 建立內容
+img1 = Image.open('gura.jpg')
+img_1 = ImageTk.PhotoImage(img1)
+tree.insert('', index=END, text=' gura', image=img_1,
+            values=('1380000', '1000', '5000000000000'))
+img1 = Image.open('gura.jpg')
+img_2 = ImageTk.PhotoImage(img1)
+tree.insert('', index=END, text=' gura', image=img_2,
+            values=('1380000', '1000', '5000000000000'))
+img1 = Image.open('gura.jpg')
+img_3 = ImageTk.PhotoImage(img1)
+tree.insert('', index=END, text=' gura', image=img_3,
+            values=('1380000', '1000', '5000000000000'))
+img1 = Image.open('gura.jpg')
+img_4 = ImageTk.PhotoImage(img1)
+tree.insert('', index=END, text=' gura', image=img_4,
+            values=('1380000', '1000', '5000000000000'))
+# 建立視窗
+tree.pack()
 # 固定視窗
 root.resizable(width=0, height=0)
 root.mainloop()
