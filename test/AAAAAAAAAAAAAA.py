d92db0ab719efa1ce874486f98db37fd5d80222d
powpow <andy20070518@gmail.com>
2020-11-19 10:28:31 +0800

    update
    AAAAAAAAAAAAAAAAA

--- test/AAAAAAAAAAAAAA.py
+++ test/AAAAAAAAAAAAAA.py
@@ -1,23 +1,32 @@
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.scrolledtext import ScrolledText
+from tkinter import ttk
+
 
 
 root = Tk()
 root.config(bg="white")
-
-root.geometry('%dx%d' % (500, 150*2))  # 設定視窗大小
+# 設定視窗大小
+root.geometry('%dx%d' % (500, 150*2))
+# 分割線
+sh = ttk.Separator(root,orient='horizontal')
+sh.grid(row=0, ipadx=300, column=0, columnspan=1000, sticky="we")
+# 第一章圖
 channel_img = Image.open('gura.jpg')    
 channel_img = channel_img.resize((100, 100), Image.ANTIALIAS)
 channel_photo0 = ImageTk.PhotoImage(channel_img)
 Label(root, image=channel_photo0,).grid(
-    row=0, column=0, rowspan=2, columnspan=2, padx=25, pady=25)
-
+    row=1, column=0, rowspan=2, columnspan=2, padx=25, pady=25)
+# 分割線
+sh = ttk.Separator(root,orient='horizontal')
+sh.grid(row=3, ipadx=300, column=0, columnspan=1000, sticky="we")
+# 第二章圖
 channel_img = Image.open('gura.jpg')    
 channel_img = channel_img.resize((100, 100), Image.ANTIALIAS)
 channel_photo1 = ImageTk.PhotoImage(channel_img)
 Label(root, image=channel_photo1,).grid(
-    row=2, column=0, rowspan=2, columnspan=2, padx=25, pady=25)
+    row=4, column=0, rowspan=2, columnspan=2, padx=25, pady=25)
 
 
 root.resizable(width=0, height=0)  # 固定視窗
